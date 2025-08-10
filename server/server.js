5f7055f95e37f1b36a1d00fc210cacad71ec388a
puniiith25 <puniiith25@gmail.com>
2025-08-10 20:25:26 +0530

    Know working on backend

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+    res.send('sever is Live!')
+})
+
+
+app.listen(PORT, () => {
+    console.log(`server is runnin on http://localhost:${PORT}`);
+})
