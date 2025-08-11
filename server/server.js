0b6e2ae014d167a0857fb16d2bb11205e9bb02e1
puniiith25 <puniiith25@gmail.com>
2025-08-11 20:43:46 +0530

    backend running successfull with expreess server

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 
+import { clerkMiddleware } from '@clerk/express'
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(clerkMiddleware())
 
 
 const PORT = process.env.PORT || 8000;
